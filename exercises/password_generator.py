9db433a88436c38ddbc33214bc5c5c676dd4fa99
dslsyeoh <syeoh91x@gmail.com>
2019-10-13 23:36:36 +0800

    enhance password generator does not repetitive same group more than 3 times

--- exercises/password_generator.py
+++ exercises/password_generator.py
@@ -6,9 +6,9 @@ from exercises.utils.password_generator_util import generate_password
 
 
 print("\n======================= PASSWORD GENERATOR =======================")
-print("Password security level: [E]asy, [M]edium, [H]ard")
 password_length = int(input("Enter your password length: "))
-security_level = input("Enter your security level: ").upper()
+print("Password security level: [E]asy, [M]edium, [H]ard")
+security_level = input("Enter your password security level: ").upper()
 if security_level is None:
     print("Password security level incorrect!, Please try again.")
 else:
--- exercises/utils/password_generator_util.py
+++ exercises/utils/password_generator_util.py
@@ -9,18 +9,40 @@ punctuations = string.punctuation
 criteria_list = [numbers, lowercase_words, uppercase_words, punctuations]
 security_level_dict = {"E": 2, "M": 3, "H": 4}
 
+REPETITIVE_LIMIT = 3
+
+
+def generate_password(password_length, security_level):
+    generated_words = []
+    old_group_index = None
+    password_groups = get_password_groups(security_level)
+    repetitive_count = 1
+    for n in range(0, password_length):
+        group_index = randomize(password_groups)
+        repetitive_count = repetitive_counter_increment(old_group_index, group_index, repetitive_count)
+        if repetitive_count == REPETITIVE_LIMIT:
+            group_index = password_algorithm(password_groups, old_group_index, group_index)
+            repetitive_count = 0
+        group_items = password_groups[group_index]
+        generated_words.append(group_items[randomize(group_items)])
+        old_group_index = group_index
+    return ''.join(map(str, generated_words))
+
 
 def get_password_groups(security_level):
     return [criteria_list[index] for index in range(0, get_security_level(security_level))]
 
 
-def generate_password(password_length, security_level):
-    generated_words = []
-    password_groups = get_password_groups(security_level)
-    for n in range(0, password_length):
-        group_items = password_groups[randomize(password_groups)]
-        generated_words.append(group_items[randomize(group_items)])
-    return ''.join(map(str, generated_words))
+def repetitive_counter_increment(old_group_index, group_index, counter):
+    if old_group_index is not None and old_group_index == group_index:
+        return counter + 1
+    return counter
+
+
+def password_algorithm(password_groups, old_group_index, group_index):
+    while old_group_index is not None and old_group_index == group_index:
+        group_index = randomize(password_groups)
+    return group_index
 
 
 def randomize(item):
